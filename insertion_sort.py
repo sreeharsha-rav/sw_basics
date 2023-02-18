7e5e93dd9d50e196172e43f90f7d4bc42a19576e
Sreeharsha98 <sreebiz98@gmail.com>
2023-02-18 07:57:52 -0700

    added insertion_sort

---
+++ insertion_sort.py
@@ -0,0 +1,18 @@
+def insertion_sort(arr):
+    # sorts input array in ascending order
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        # shift all elements towards right to insert key
+        while j >= 0 and key < arr[j]:
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key  # insert key
+    
+    return arr 
+
+# Driver code
+if __name__ == "__main__":
+    num_list = [int(num) for num in input("Enter integer array: ").split()]
+    sort_list = insertion_sort(num_list)
+    print("Sorted array: ", sort_list)
